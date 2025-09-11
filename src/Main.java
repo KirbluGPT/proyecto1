e76de07c8f2af1dee05cd4f3b18c88eaf300e6c7
Sxz3 <redjaxy28@gmail.com>
2025-09-11 12:16:06 +0200

    Carusel

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,27 @@ public class Main {
         //pon tu opcion
 
         System.out.println("Opcion 1: caca");
+        System.out.println("Opcion 2: carusel");
 
 
         System.out.print("Opcion: ");
-        switch (sc.nextInt) {
+        switch (sc.nextInt()) {
 
             case 1: {
+
+            }
+            case 2: {
+                sc.nextLine();
+                System.out.println("¿Que es la vida?");
+                String ans = sc.nextLine().toLowerCase();
+
+                if (ans.equalsIgnoreCase("un carusel")){
+                    System.out.println("Correcto");
+                    break;
+                } else {
+                    System.out.println("Incorrecto. Esta accion tendra consecuencias.");
+                    break;
+                }
 
             }
         }
