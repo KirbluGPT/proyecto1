f12d2697b217d27adc6b2a211a800bd363268d15
marfonbro <marfonbro@alu.edu.gva.es>
2025-09-11 12:07:16 +0200

    yayaya

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("menu: ");
+
+        //pon tu opcion
+
+        System.out.println("Opcion 1: caca");
+
+
+        System.out.print("Opcion: ");
+        switch (sc.nextInt) {
+
+            case 1: {
+
+            }
+        }
+    }
+}
